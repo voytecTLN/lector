5df113567a1e561e2b7b3a381284936e7a226bef
wokisajn <w0jt4ss.p@gmail.com>
2025-06-30 12:56:14 +0200

    Rename and ignore `index.php` in project root
    Moved `index.php` to the public directory for better structure and alignment with modern frameworks. Updated `.gitignore` to exclude `index.php` in the root directory to avoid conflicts.
    
    On local standard Laravel application, on production changes required for hosting.

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
